85edd6e655e36adc509e93111bbc842cbd3e9145
pranav0904 <pranavkhiste1@gmail.com>
2020-11-23 14:01:33 -0500

    UI updated
    tkinter used for UI

--- code.py
+++ code.py
@@ -1,166 +1,211 @@
+#!/usr/bin/env python
+import os
 import pandas as pd
 import numpy as np
 from error import rmse
+
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsClassifier
 
-
-data = pd.read_csv("Data.csv")
-
-
-''' Matrix initialization '''
-col = 3
-row = 943 * 1682
-
-Initiated_Array = np.zeros((row,col))
-#print(Initiated_Array)
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
 
 
 
-'''For Normalization '''
-min_rating = min(data["Rating"]) 
-max_rating = max(data["Rating"])
+class Root(Tk):
+    def __init__(self):
+        super(Root,self).__init__()
+        self.title("Recommenders System")
+        self.minsize(640,400)
+        self.configure(background = "#A9A9A9")
+        self.labelFrame = ttk.LabelFrame(self, text = "------Recommenders System------")
+        self.labelFrame.grid(column = 0, row = 1, padx = 20, pady = 20)
+        self.textarea = tk.Text(self)
+        self.textarea.grid(column = 0, row = 8)
+        global n_flag
+        self.n_flag = False
+        self.button_find_file()
+        self.labelFrame = ttk.LabelFrame(self, text = "****** Algorithms ******")
+        self.labelFrame.grid(column = 0, row = 3, padx = 20, pady = 20)
+        self.button_LinearRegression()
+        self.button_RandomForestRegressor()
+        self.button_GenerateOutput()
+        self.button_exit()
+
+    def button_find_file(self):
+        self.button = ttk.Button(self.labelFrame, text = "Import a File", command = self.fileDialog)
+        self.button.grid(column = 1, row = 1)
+
+    def button_LinearRegression(self):
+        self.button = ttk.Button(self.labelFrame, text = "Linear Regression", command = self.LinearRegression)
+        self.button.grid(column = 0, row = 3)
+
+    def button_RandomForestRegressor(self):
+        self.button = ttk.Button(self.labelFrame, text = "Random Forest Regressor", command = self.RandomForestRegressor)
+        self.button.grid(column = 0, row = 4)
+
+    def button_GenerateOutput(self):
+        self.button = ttk.Button(self.labelFrame, text = "Generate Output file using Random Forest Regressor", command = self.GenerateOutput)
+        self.button.grid(column = 0, row = 5)
+
+    def button_exit(self):
+        self.button = ttk.Button(self.labelFrame, text = "Exit", command = self.close_window)
+        self.button.grid(column = 0, row = 7)
+
+    def close_window(self):
+        self.destroy()
+
+    def fileDialog(self):
+        global filename
+        self.filename = filedialog.askopenfilename(initialdir =  "/", title = "Select A File", filetypes = (("csv files","*.csv"),("all files","*.*")) )
+        self.label = ttk.Label(self.labelFrame, text = "")
+        self.label.grid(column = 1, row = 2)
+        self.import_content()
+        self.DataSplit()
+        if self.flag == True :
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "CSV File imported successfully!")
+        else:
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "Some error occured!")
+
+    def import_content(self):
+        global data
+        self.flag = False
+        cdir = os.path.dirname(__file__)
+        file_res = os.path.join(cdir, self.filename)
+        data = pd.read_csv(file_res)
+        self.flag = True
+
+    def matrix_init(self):
+        users = 943
+        items = 1682
+        col = 3
+        matrix_initialisation = np.zeros(users * items, col)     #Matrix initialisation with 0's
 
 
+    def DataSplit(self):
+        '''For Normalization '''
+        global train_x, test_x, train_y, test_y
+        min_rating = min(data["Rating"])
+        max_rating = max(data["Rating"])
 
-X=data[['User','Item']]      #Independent Variables
-y=data['Rating']
+        X = data[['User','Item']]      #Independent Variables
 
-'''Min-Max Normalization on Y'''
-Y=data['Rating'].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values   #Dependent variable
+        '''Min-Max Normalization on Y'''
+        Y = data['Rating'].apply(lambda x: (x - min_rating) / (max_rating - min_rating)).values   #Dependent variable
 
+        train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.2, random_state=42)
+        self.n_flag = True
 
-train_x, test_x, train_Y, test_Y = train_test_split(X, Y, test_size=0.2, random_state=42)
+    def DataSplit2(self):
+        '''For Normalization '''
+        global train_x, test_x, train_y, test_y
+        min_rating = min(data["Rating"])
+        max_rating = max(data["Rating"])
 
+        X = data[['User','Item']]      #Independent Variables
+        y=data['Rating']
 
-'''1. LinearRegression Model'''
-print("\n\n")
-print("**************** LinearRegression Model ****************")
-reg = LinearRegression()
-reg.fit(train_x, train_Y)
+        train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.n_flag = True
 
-predicted_L = reg.predict(test_x)
-print('Model Score: ',reg.score(test_x, test_Y))
+    def LinearRegression(self):
+        if self.n_flag == True :
+            '''1. LinearRegression Model'''
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "**************** LinearRegression Model ****************\n")
 
-'''RMSE Value for Predicted'''
-print('RMSE value for LinearRegression Model : ', rmse(predicted_L, test_Y))
+            reg = LinearRegression()
+            reg.fit(train_x, train_y)
+            predicted_L = reg.predict(test_x)
+            text = 'Model Score: ' + str(reg.score(test_x, test_y))
+            self.textarea.insert(END, text + "\n")
+            '''RMSE Value for Predicted'''
+            text = 'RMSE value for LinearRegression Model : ' + str(rmse(predicted_L, test_y))
+            self.textarea.insert(END, text + "\n")
+        else:
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "Import CSV file first!")
 
+    def RandomForestRegressor(self):
+        if self.n_flag == True :
+            '''2. Random Forest Model'''
 
-'''
-users =  943
-items = 1682
-ratings = 0#1682
+            self.MsgBox = tk.messagebox.askyesno ('Clear TextArea','Do you want to clear TextArea?\n')
+            if self.MsgBox == True:
+                self.textarea.delete("1.0","end")
+                self.textarea.insert(END, "**************** Random Forest Model ****************\n")
+            else:
+                self.textarea.insert(END, "**************** Random Forest Model ****************\n")
 
-print("\n")
-print("Predicting the values....")
-Final_Ratings_1 = []
+            regr_R = RandomForestRegressor(max_depth=2, random_state=0)
+            regr_R.fit(train_x, train_y)
 
-for i in range(845,users+1):    
-    for j in range(1500,items+1):
-        rating = reg.predict([[i,j]])
-        Actual_rating = ( rating * (max_rating - min_rating)) + min_rating
-        Final_Ratings_1.append(int(Actual_rating))
+            predicted_R = regr_R.predict(test_x)
+            text = 'Model Score: ' + str(regr_R.score(test_x, test_y))
+            self.textarea.insert(END, text + "\n")
 
-print("\n")
-print(Final_Ratings_1)      
-'''
+            '''RMSE Value for Predicted'''
+            text = 'RMSE value for Random Forest Model : ' + str(rmse(predicted_R, test_y))
+            self.textarea.insert(END, text + "\n")
+        else:
+            self.textarea.delete("1.0","end")
+            self.textarea.insert(END, "Import CSV file first!\n")
 
+    def rmse_cal(self):
+        self.DataSplit()
+        regr_R = RandomForestRegressor(max_depth=2, random_state=0)
+        regr_R.fit(train_x, train_y)
 
+        predicted_R = regr_R.predict(test_x)
+        text = 'Model Score: ' + str(regr_R.score(test_x, test_y))
+        self.textarea.insert(END, text + "\n")
 
+        '''RMSE Value for Predicted'''
+        text = 'RMSE value for Random Forest Model : ' + str(rmse(predicted_R, test_y))
+        self.textarea.insert(END, text + "\n")
 
-'''2. Random Forest Model'''
-print("\n\n")
-print("**************** Random Forest Model ****************")
-regr_R = RandomForestRegressor(max_depth=2, random_state=0)
-regr_R.fit(train_x, train_Y)
+    def GenerateOutput(self):
+        self.DataSplit2()
+        self.textarea.insert(END, "\n\n**************** Random Forest Model ****************\n")
+        neigh = KNeighborsClassifier()
+        neigh.fit(train_x, train_y)
 
-predicted_R = regr_R.predict(test_x)
-print('Model Score: ',regr_R.score(test_x, test_Y))
+        predicted_K = neigh.predict(test_x)
+        text = 'Model Score: ' + str(neigh.score(test_x, test_y))
+        self.textarea.insert(END, text + "\n")
 
-'''RMSE Value for Predicted'''
-print('RMSE value for Random Forest Model : ', rmse(predicted_R, test_Y))
-print("\n\n")
+        self.rmse_cal()
 
+        self.DataSplit2()
 
-'''
-The matrix has 943 users (m = 943) and 1682 items (n = 1682).
-Your goal is to fill out all the entries of this matrix with the current value as 0 (or no value yet). 
+        users =  943 #1...943
+        items = 1682 #1...1682
+        ratings = 0  #1...5
 
-'''
-'''
-users =  943
-items = 1682
-ratings = 0#1682
+        file_output = open("Predicted_values.txt", "w+")
 
-print("\n")
-print("Predicting the values....")
-Final_Ratings = []
+        self.textarea.insert(END, "Predicting the ratings given for 943 users and 1682 items ....")
+        self.textarea.insert(END, "\n\n\t\tWait time 12 mins or less...\n")
+        self.MsgBox = tk.messagebox.showinfo("Generated Output File", "Predicted the ratings given for 943 users and 1682 items ....\nOUTPUT File name: Predicted_values.txt")
 
-for i in range(845,users+1):    
-    for j in range(1500,items+1):
-        rating = regr_R.predict([[i,j]])
-        Actual_rating = ( rating * (max_rating - min_rating)) + min_rating
-        Final_Ratings.append(int(Actual_rating))
+        Predicted_Ratings = []
 
-print("\n")
-print(Final_Ratings)      
-'''
-#Actual_Y = (model.predict([[209,260]]) * (max_rating - min_rating)) + min_rating
+        for i in range(1,users+1):
+            for j in range(1,items+1):
+                rating = neigh.predict([[i,j]])
+                val = str(i)+" "+str(j)+" "+str(rating[0]) + "\n"
+                file_output.write(val)
 
+        self.MsgBox = tk.messagebox.showinfo("Done", "File Generated with name Predicted_values.txt!")
+        file_output.close()
 
-'''3. Logistic Regression Model'''
-
-
-train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=42) # Without Normalization 
-
-print("\n\n")
-print("**************** Logistic Regression Model ****************")
-l_reg = LogisticRegression(random_state=0)
-l_reg.fit(train_x, train_y)
-
-predicted_l_reg = l_reg.predict(test_x)
-print('Model Score: ',l_reg.score(test_x, test_y))
-
-'''RMSE Value for Predicted'''
-print('RMSE value for Random Forest Model : ', rmse(predicted_l_reg, test_y))
-print("\n\n")
-
-
-
-
-'''4. KNN Clustering Classifier '''
-
-print("\n\n")
-print("**************** KNN Model ****************")
-neigh = KNeighborsClassifier()
-neigh.fit(train_x, train_y)
-
-predicted_K = neigh.predict(test_x)
-print('Model Score: ',neigh.score(test_x, test_y))
-
-'''RMSE Value for Predicted'''
-print('RMSE value for Random Forest Model : ', rmse(predicted_K, test_y))
-print("\n\n")
-
-
-
-users =  10 #943
-items = 15 #1682
-ratings = 0#1682
-
-print("\n")
-print("Predicting the ratings given for 15 items by 10 users  ....")
-Predicted_Ratings = []
-
-for i in range(1,users+1):    
-    for j in range(1,items+1):
-        rating = neigh.predict([[i,j]])
-        #Actual_rating = ( rating * (max_rating - min_rating)) + min_rating
-        Predicted_Ratings.append(int(rating))
-
-print("\n")
-print("Predicted Ratings are : ",Predicted_Ratings)
-print("\n")
+if __name__ == '__main__':
+    root = Root()
+    root.mainloop()
